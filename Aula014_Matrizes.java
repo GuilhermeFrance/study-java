4448e19ea82b693f5d994b677c77eefc16eee50b
Guilherme <guilhermefrance9@gmail.com>
2026-02-12 15:54:16 -0300

    finalize-matrix

--- Aula014_Matrizes.java
+++ Aula014_Matrizes.java
@@ -12,11 +12,28 @@ public class Aula014_Matrizes {
         int n = sc.nextInt();
         int[][] mat = new int[n][n];
 
-        for(int i = 0; i<n; i++){
+        for(int i = 0; i<mat.length; i++){
             for(int j = 0; j<n; j++){
                 mat[i][j] = sc.nextInt();
             }
         }
 
+        System.out.println("Main diagonal: ");
+
+        for(int i = 0; i<mat.length; i++){
+            System.out.println(mat[i][i] + " ");
+        }
+
+        int count = 0;
+        for(int i = 0; i<mat.length; i++){
+            for(int j = 0; j < mat.length; j++){
+                if(mat[i][j] < 0){
+                    count++;
+                }
+            }
+        }
+
+        System.out.println("Negative numbers: " + count);
+        sc.close();
     }
 }
